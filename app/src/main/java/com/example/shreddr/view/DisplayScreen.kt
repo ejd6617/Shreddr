19a8411c52701cc7a80039343292543e2c915b1a
Aarick Zook <aarickzook@gmail.com>
2024-12-12 00:49:29 -0500

    Add clickable chords

--- app/src/main/java/com/example/shreddr/view/DisplayScreen.kt
+++ app/src/main/java/com/example/shreddr/view/DisplayScreen.kt
@@ -3,10 +3,12 @@ package com.example.shreddr.view
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -22,12 +24,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
 import androidx.navigation.NavController
 import com.example.shreddr.controller.ChordChartController.Companion.getSelectedChordChart
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.IconButton
+import androidx.compose.foundation.shape.RoundedCornerShape
 
 class DisplayScreen(private val navController: NavController) {
 
@@ -83,7 +87,16 @@ class DisplayScreen(private val navController: NavController) {
                             horizontalArrangement = Arrangement.spacedBy(8.dp)
                         ) {
                             Column{
-                                Text(pair.chords, color = Color.Black)
+                                Box(
+                                    modifier = Modifier
+                                        .background(Color.LightGray, shape = RoundedCornerShape(4.dp))
+                                        .padding(8.dp)
+                                        // .clickable { selectedChord = pair.chords }
+                                        .clickable {},
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(pair.chords, color = Color.Black)
+                                }
                                 Text(pair.lyrics, color = Color.Black)
                             }
                         }
